fbb9e52582a73f085168988d51fb458e38ad7024
Anwaripasha Akbar <pashaakbar52@gmail.com>
2022-10-16 20:28:29 +0700

    Penambahan Navbar dan Footer

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -1,32 +1,36 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid container">
-        <a class="navbar-brand" href="#">Pemerintah Provinsi Sumatera Selatan</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+        <img class="" src="" alt="" style="width:30px">
+        <a class="navbar-brand" href="/dashboard/index.html">Pemerintah Provinsi
+            Sumatera Selatan</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="/">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/About">Features</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Surat Masuk-Keluar
-              </a>
-              <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <li><a class="dropdown-item" href="/agenda-surat-masuk">Surat Agenda Masuk</a></li>
-                <li><a class="dropdown-item" href="/surat-keluar">Surat keluar</a></li>
-                <li><a class="dropdown-item" href="/surat-tugas">Surat Tugas Masuk</a></li>
-                <li><a class="dropdown-item" href="/surat-sekda">Surat Sekda Keluar</a></li>
-                <li><a class="dropdown-item" href="/surat-masuk-bkd">Surat BKD Masuk</a></li>
-                <li><a class="dropdown-item" href="surat-masuk-bkd-iii">Surat BKD III Masuk</a></li>
-                <li><a class="dropdown-item" href="/surat-keluar-bkd">Surat BKD Keluar</a></li>
-              </ul>
-            </li>
-          </ul>
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/About">About</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Surat Masuk-Keluar
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a class="dropdown-item" href="/agenda-surat-masuk">Surat Agenda Masuk</a></li>
+                        <li><a class="dropdown-item" href="/surat-keluar">Surat keluar</a></li>
+                        <li><a class="dropdown-item" href="#">Surat Tugas Masuk</a></li>
+                        <li><a class="dropdown-item" href="#">Surat Sekda Keluar</a></li>
+                        <li><a class="dropdown-item" href="#">Surat BKD Masuk</a></li>
+                        <li><a class="dropdown-item" href="#">Surat BKD III Masuk</a></li>
+                        <li><a class="dropdown-item" href="#">Surat BKD Keluar</a></li>
+                    </ul>
+                </li>
+            </ul>
         </div>
-      </div>
-  </nav>
+    </div>
+</nav>
